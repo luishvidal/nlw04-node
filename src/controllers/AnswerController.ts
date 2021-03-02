03dca276afdd513abe6ce1f134c6a84dc617c51f
Luís Vidal <luis.vidal206@gmail.com>
2021-03-02 14:08:51 -0300

    API finalizada

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { AppError } from '../errors/AppError';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+class AnswerController {
+
+    // http://localhost:3333/answers/1?u=4fd60304-38d6-4ed6-b491-144e3b5a3ecc
+
+    async execute(request: Request, response: Response) {
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u),
+        });
+
+        if(!surveyUser) {
+            throw new AppError("Survey user does not exists");
+        }
+
+        surveyUser.value = Number(value);
+
+        await surveysUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser);
+    }
+}
+
+export { AnswerController };
